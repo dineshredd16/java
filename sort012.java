b77eab0965b7f3f64f12cd1df8959df037340d64
dineshredd16 <dinesh.reddem124@gmail.com>
2023-01-29 02:34:55 -0500

    update

--- sort012.java
+++ sort012.java
@@ -4,6 +4,9 @@ public class sort012 {
   public static void main(String[] args){
     Scanner readUser = new Scanner(System.in);
     System.out.print("enter the number of test cases: ");
+    int zero = 0;
+    int one = 0;
+    int two = 0;
     int t = readUser.nextInt();
     for(int i=0; i<t; i++){
       System.out.print("enter the size of the array: ");
@@ -12,16 +15,38 @@ public class sort012 {
       for(int j=0; j<n; j++){
         System.out.print("enter the element: ");
         array[j] = readUser.nextInt();
-      }
-      for(int j=0; j<n; j++){
-        for(int k=j+1; k<n; k++){
-          if(array[j] > array[k]){
-            int temp = array[j];
-            array[j] = array[k];
-            array[k] = temp;
-          }
+        if(array[j] == 0){
+          zero = zero + 1;
+        }
+        else if(array[j] == 1){
+          one = one + 1;
+        }
+        else {
+          two = two + 1;
         }
       }
+      for(int j=0; j<n; j++){
+        if(j < zero){
+            array[j] = 0;
+        }
+        else if(j< zero + one){
+            array[j] = 1;
+        }
+        else if(j< zero + one + two){
+            array[j] = 2;
+        }
+      }
+
+      // for(int j=0; j<n; j++){
+      //   for(int k=j+1; k<n; k++){
+      //     if(array[j] > array[k]){
+      //       int temp = array[j];                 // very very slow method useless!!!!!
+      //       array[j] = array[k];                 // dont use such solutions
+      //       array[k] = temp;
+      //     }
+      //   }
+      // }
+      
       for(int j=0; j<n; j++){
         System.out.print(array[j] + " ");
       }
